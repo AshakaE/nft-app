e03c317c5b28d6cc3087ec832e16dea229477c43
AshakaE <ashakaegerega@gmail.com>
2022-08-07 11:17:01 +0100

    nft app

---
+++ constants/index.js
@@ -0,0 +1,22 @@
+export const abi = [
+    {
+        inputs: [
+            {
+                internalType: 'address',
+                name: '',
+                type: 'address',
+            },
+        ],
+        name: 'whitelistedAddresses',
+        outputs: [
+            {
+                internalType: 'bool',
+                name: '',
+                type: 'bool',
+            },
+        ],
+        stateMutability: 'view',
+        type: 'function',
+    },
+]
+export const NFT_CONTRACT_ADDRESS = '0x18Fb0e8677626061318A8CCb5d9aBCB4FBB89e7F'
